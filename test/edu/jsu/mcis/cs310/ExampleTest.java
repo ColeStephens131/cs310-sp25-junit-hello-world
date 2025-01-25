0ad65ef1067947eae9e0701f3bfc21494861a1e0
Cole Stephens <colestephens131@gmail.com>
2025-01-25 17:34:49 -0600

    Added a new Test method.

--- test/edu/jsu/mcis/cs310/ExampleTest.java
+++ test/edu/jsu/mcis/cs310/ExampleTest.java
@@ -7,7 +7,8 @@ public class ExampleTest {
 
     private Main main;
     private String expectedGreeting, expectedReverseGreeting;
-    private String expectedReverse1, expectedReverse2, expectedReverse3;
+    //Added expectedReverse4
+    private String expectedReverse1, expectedReverse2, expectedReverse3, expectedReverse4;
     
     @Before
     public void setUp() {
@@ -17,6 +18,8 @@ public class ExampleTest {
         expectedReverse1 = "topaeT elttiL a m'I";
         expectedReverse2 = "elddiD elddiD yeH";
         expectedReverse3 = "kcoD yrokciD yrokciH";
+        //added the reverse String for expectedReverse4
+        expectedReverse4 = ".nosrep a s'nosrep A";
     }
         
     @Test
@@ -47,6 +50,13 @@ public class ExampleTest {
     public void testReverseMessage3() {
         String actual = main.reverse("Hickory Dickory Dock");
         assertEquals(expectedReverse3, actual);
+    }
+    
+    //Added a new Test method
+    @Test
+    public void testReverseMessage4() {
+        String actual = main.reverse("A person's a person.");
+        assertEquals(expectedReverse4, actual);
     }
     
 }
