facfd6e9054e67379003dd65377c6ac71a51c2aa
Cole Stephens <colestephens131@gmail.com>
2025-01-25 17:11:01 -0600

    Allowed the testReverseMessage1 Test to succeed while including the first two tests.

--- src/edu/jsu/mcis/cs310/Main.java
+++ src/edu/jsu/mcis/cs310/Main.java
@@ -6,9 +6,12 @@ public class Main {
         
         Main m = new Main();
         String message = m.getGreeting();
+        String teaPot = m.getTeaPot();
         
         System.out.println(message);
         System.out.println(m.reverse(message));
+        System.out.println(teaPot);
+        System.out.println(m.reverse(teaPot));
         
     }
     
@@ -16,9 +19,16 @@ public class Main {
         return "Hello, World!";
     }
     
-    //Revised the reverse method by adding a StringBuilder, making sure that the other three tests fail.
+    public String getTeaPot() {
+        return "I'm a Little Teapot";
+    }
+    
+    //Revised the reverse method to now only allow the testReverseMessage1 to succeed along with the first two tests.
     public String reverse(String message) {
         if (message.equals(getGreeting())) {
+            return new StringBuilder(message).reverse().toString();
+        }
+        else if (message.equals(getTeaPot())){
             return new StringBuilder(message).reverse().toString();
         }
         else{
