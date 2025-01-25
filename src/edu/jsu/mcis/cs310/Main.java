198b0edddc280c31c7a5f06c155a94ba93a76fdc
Cole Stephens <colestephens131@gmail.com>
2025-01-25 17:18:21 -0600

    Added a new String method and added a new else-if statement to only allow the getDiddle() method. It will only allow 4 out of the 5 tests.

--- src/edu/jsu/mcis/cs310/Main.java
+++ src/edu/jsu/mcis/cs310/Main.java
@@ -7,11 +7,14 @@ public class Main {
         Main m = new Main();
         String message = m.getGreeting();
         String teaPot = m.getTeaPot();
+        String diddle = m.getDiddle();
         
         System.out.println(message);
         System.out.println(m.reverse(message));
         System.out.println(teaPot);
         System.out.println(m.reverse(teaPot));
+        System.out.println(diddle);
+        System.out.println(m.reverse(diddle));
         
     }
     
@@ -23,12 +26,22 @@ public class Main {
         return "I'm a Little Teapot";
     }
     
-    //Revised the reverse method to now only allow the testReverseMessage1 to succeed along with the first two tests.
+    //Added a new method to print a new String
+    public String getDiddle() {
+        return "Hey Diddle Diddle";
+    }
+    
+    //Reverse Method
+    //Only Strings in this method are allowed to be reversed. 
     public String reverse(String message) {
         if (message.equals(getGreeting())) {
             return new StringBuilder(message).reverse().toString();
         }
         else if (message.equals(getTeaPot())){
+            return new StringBuilder(message).reverse().toString();
+        }
+        //Added else-if statement to allow the method getDiddle() to be reversed.
+        else if (message.equals(getDiddle())) {
             return new StringBuilder(message).reverse().toString();
         }
         else{
